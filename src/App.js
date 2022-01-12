d13dbc96d3b0485d62a09e80cd9741a19382b53f
GabrielLiima <gabrielliima.dev@gmail.com>
2022-01-12 16:55:38 -0300

    create 'Expense' component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import classes from "./App.module.css";
 import Header from "./components/expenses/Header";
 import Card from "./components/UI/Card";
+import Expense from "./components/expenses/Expense";
 
 function App() {
   return (
     <Card className={classes.container}>
       <Header />
+      <Expense />
     </Card>
   );
 }
---
+++ src/components/expenses/Expense.js
@@ -0,0 +1,19 @@
+import classes from "./Expense.module.css";
+import Card from "../UI/Card";
+
+const Expense = () => {
+  return (
+    <Card className={classes.wrapper}>
+      <div className={classes.income}>
+        <h2>INCOME</h2>
+        <p>$500.00</p>
+      </div>
+      <div className={classes.expense}>
+        <h2>EXPENSE</h2>
+        <p>$240.00</p>
+      </div>
+    </Card>
+  );
+};
+
+export default Expense;
