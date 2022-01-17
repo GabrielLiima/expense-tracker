77c26a4e572257aeb6fb1ed7cd1282bd30bac429
GabrielLiima <gabrielliima.dev@gmail.com>
2022-01-16 21:10:59 -0300

    bug fix

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
+
 import classes from "./Item.module.css";
 import Card from "../UI/Card";
 
 const Item = (props) => {
+  const [deleteMode, setDeleteMode] = useState(false);
+
   const deleteHandler = () => {
-    props.onDeleteItem(props.id);
-  }
+    if (deleteMode) {
+      props.onDeleteItem(props.id);
+    }
+  };
+
+  const enterDeleteMode = () => {
+    setTimeout(function () {
+      setDeleteMode(true);
+    }, 0);
+  };
+
+  const leaveDeleteMode = () => {
+    setTimeout(function () {
+      setDeleteMode(false);
+    }, 0);
+  };
 
   return (
-    <li>
+    <li onMouseEnter={enterDeleteMode} onMouseLeave={leaveDeleteMode}>
       <Card className={`${props.income ? classes.income : classes.expense}`}>
-        <button className={classes.delete} onClick={deleteHandler}>x</button>
+        {deleteMode && (
+          <button className={classes.delete} onClick={deleteHandler}>
+            x
+          </button>
+        )}
         <div className={classes.title}>{props.title}</div>
         <div className={classes.amount}>
           {`${props.income ? "+" : "-"}${props.amount
